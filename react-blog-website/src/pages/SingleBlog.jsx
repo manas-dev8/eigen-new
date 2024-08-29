48f63707ea167d8ec887c5619fea9ac3921fca8d
manassaxena008 <manassaxena9719@gmail.com>
2024-08-30 01:34:14 +0530

    change

--- react-blog-website/src/pages/SingleBlog.jsx
+++ react-blog-website/src/pages/SingleBlog.jsx
@@ -7,7 +7,7 @@ import blogsdata from "../data/blogsdata.json";
 const SingleBlog = () => {
     const { id } = useParams();
     const data = blogsdata.blogs.find(blogs => blogs.id === parseInt(id)); 
-    const { image, title, author, published_date, reading_time, content } = data;
+    const { image, title, author, published_date, reading_time, content1,content2, content3, content4, content5,content6, content7, content8, content9, content10 } = data;
 
     return (
         <div>
@@ -25,7 +25,14 @@ const SingleBlog = () => {
                     <p className='mb-3 text-gray-600'>
                         <FaClock className='inline-flex items-center mr-2' /> {reading_time}
                     </p>
-                    <p className='text-base lg:text-lg text-gray-500 mb-6'>{content}</p>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content1}</div>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content2}</div>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content3}</div>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content4}</div>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content5}</div>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content6}</div>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content7}</div>
+                    <div className='text-base lg:text-lg text-gray-500 mb-6'>{content8}</div>
                 </div>
                 <div className='lg:w-1/4'>
                     <SideBar />
