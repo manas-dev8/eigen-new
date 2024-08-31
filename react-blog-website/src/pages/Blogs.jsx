4f07ef03ad6ebddf760d6b79bcc51753c8afe065
manassaxena008 <manassaxena9719@gmail.com>
2024-08-31 18:42:12 +0530

    change in blog page

--- react-blog-website/src/pages/Blogs.jsx
+++ react-blog-website/src/pages/Blogs.jsx
@@ -4,10 +4,8 @@ import BlogPage from '../components/BlogPage'
 const Blogs = () => {
   return (
     <div>
-      <div className='py-40 bg-gray-500 text-center text-white px-4'>
-      <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Our Blog page</h1>
+      <div className=' bg-gray-500 text-center text-white px-4 py-8 mx-4'>
       </div>
-      {/* all blogs container */}
       <div className='max-w-7xl mx-auto'>
         <BlogPage/>
       </div>
